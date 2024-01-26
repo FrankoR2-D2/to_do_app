0043f01d8563e31416e99b3f21899b3ebb893dcd
FrankoR2-D2 <frankoreynolds.fr@gmail.com>
2024-01-26 13:04:35 +0200

    finished with todo project

--- to-do-app-ng/src/app/components/todos/todos.component.ts
+++ to-do-app-ng/src/app/components/todos/todos.component.ts
@@ -8,9 +8,13 @@ import { Todo } from 'app/models/todo';
 })
 export class TodosComponent implements OnInit {
 
+
+
   todos!:Todo[];
 
   constructor() {}
+
+  inputTodo:string = '';
 
   ngOnInit(): void{
     this.todos = [
@@ -23,5 +27,28 @@ export class TodosComponent implements OnInit {
         completed: true
       }
     ]
+  }
+
+
+  toggleDone (id: number) {
+    this.todos.map((v, i) => {
+      if( i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  deleteToDo(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+
+    this.inputTodo = '';
   }
 }
